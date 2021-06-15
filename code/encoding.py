0fa1cc4b3f1606d95e9b431ba019125af4e1efcf
ebachucph <ebach@sund.ku.dk>
2021-06-15 15:14:55 +0200

    First commit of encoding parser

---
+++ code/encoding.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+def load_scheme(filename):
+    """
+    Read in encoding scheme values into matrix.
+    """
+    aa = ['A', 'R', 'N' ,'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'X']
+    df = pd.read_csv(filename, sep='\s+', comment='#', index_col=0)
+    return df.loc[aa, aa]
+
+def encode_peptides(Xin):
+    """
+    Encode AA seq of peptides using BLOSUM50.
+    Returns a tensor of encoded peptides of shape (batch_size, MAX_PEP_SEQ_LEN, n_features)
+    """
+    encoding_scheme = load_scheme(blosum_file)
+    
+    batch_size = len(Xin)
+    n_features = len(blosum)
+    
+    Xout = np.zeros((batch_size, MAX_PEP_SEQ_LEN, n_features), dtype=np.int8) # should it be uint? is there a purpose to that?
+    
+    for peptide_index, row in Xin.iterrows():
+        for aa_index in range(len(row.peptide)):
+            Xout[peptide_index, aa_index] = encoding_scheme[ row.peptide[aa_index] ].values
+            
+    return Xout, Xin.target.values
+
