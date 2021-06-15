58fd7be28bd00c51a51445e072884e06c377e8cb
Alexander Gmeiner <algm@ALGM-MacBook-Pro.local>
2021-06-15 15:58:13 +0200

    parser function

--- code/parser.py
+++ code/parser.py
@@ -5,4 +5,12 @@ Created on Tue Jun 15 15:00:51 2021
 
 @author: algm
 """
+import pandas as pd
 
+def load_peptide_target(filename):
+    """
+    Read amino acid sequence of peptides and
+    corresponding log transformed IC50 binding values from text file.
+    """
+    df = pd.read_csv(filename, sep='\s+', usecols=[0,1], names=['peptide','target'])
+    return df
